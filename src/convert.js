24eda61cf46ecaeb3ab35ab3b2dda9b65060d743
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2016-12-15 16:06:16 +0900

    lint

--- src/convert.js
+++ src/convert.js
@@ -1,60 +1,71 @@
 /* @flow */
 
-const fs = require('fs')
-const {Observable} = require('rxjs')
-const xlsToJSON = require('xls-to-json')
+const fs = require("fs")
+const { Observable } = require("rxjs")
+const xlsToJSON = require("xls-to-json")
 
-// type Hazard = {
-//   level: number,
-//   ranking: number,
-// }
+type Hazard = {
+  level: 1 | 2 | 3,
+  ranking: number,
+}
 
-// type Place = {
-//   address: string,
-//   street: string
-// }
+type Hazards = {
+  fire: Hazard,
+  destruction: Hazard,
+  comprehension: Hazard,
+}
 
-const createConvert$ = (pathToXls: string) => Observable
+type Place = {
+  address: string,
+  street: string,
+  kind: number,
+  hazards: Hazards,
+  hazardsWithDifficality: Hazards,
+}
+
+const convert = (x: any): Place => ({
+  address: x["市区町村"],
+  street: x["町丁目名"],
+  kind: x["地盤分類"],
+  hazards: {
+    fire: {
+      level: x["火災危険度ランク"],
+      ranking: x["火災危険度順位"],
+    },
+    destruction: {
+      level: x["建物倒壊危険度ランク"],
+      ranking: x["建物倒壊危険度順位"],
+    },
+    comprehension: {
+      level: x["総合危険度ランク"],
+      ranking: x["総合危険度順位"],
+    },
+  },
+  hazardsWithDifficality: {
+    fire: {
+      level: x["災害時活動困難度を考慮した危険度火災危険度ランク"],
+      ranking: x["災害時活動困難度を考慮した危険度火災危険度順位"],
+    },
+    destruction: {
+      level: x["災害時活動困難度を考慮した危険度建物倒壊危険度ランク"],
+      ranking: x["災害時活動困難度を考慮した危険度建物倒壊危険度順位"],
+    },
+    comprehension: {
+      level: x["災害時活動困難度を考慮した危険度総合危険度ランク"],
+      ranking: x["災害時活動困難度を考慮した危険度総合危険度順位"],
+    },
+  },
+})
+
+const createConvert$ = (pathToXls: string): Observable<Place> => Observable
   .bindCallback(xlsToJSON)({
     input: pathToXls,
-    output: './assets/result.json'
+    output: "./assets/result.json",
   })
   .map(xs => xs[1])
   .map(xs => xs
     .filter((x, i) => i > 1)
-    .map(x => ({
-      address: x["市区町村"],
-      street: x["町丁目名"],
-      kind: x["地盤分類"],
-      hazards: {
-        fire: {
-          level: x["火災危険度ランク"],
-          ranking: x["火災危険度順位"],
-        },
-        destruction: {
-          level: x["建物倒壊危険度ランク"],
-          ranking: x["建物倒壊危険度順位"],
-        },
-        comprehension: {
-          level: x["総合危険度ランク"],
-          ranking: x["総合危険度順位"],
-        }
-      },
-      hazardsWithDifficality: {
-        fire: {
-          level: x["災害時活動困難度を考慮した危険度火災危険度ランク"],
-          ranking: x["災害時活動困難度を考慮した危険度火災危険度順位"],
-        },
-        destruction: {
-          level: x["災害時活動困難度を考慮した危険度建物倒壊危険度ランク"],
-          ranking: x["災害時活動困難度を考慮した危険度建物倒壊危険度順位"],
-        },
-        comprehension: {
-          level: x["災害時活動困難度を考慮した危険度総合危険度ランク"],
-          ranking: x["災害時活動困難度を考慮した危険度総合危険度順位"],
-        }
-      },
-    }))
+    .map(convert)
   )
 
 const writeEffect$ = (pathToXls: string) => (pathToResult: string) => createConvert$(pathToXls)
